0663a6f30516dfc45e5b17143f4aeb673d0a79df
Ntare Guy <gntare2@gmail.com>
2022-12-04 21:33:46 +0200

    Fix linter issues

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::GreetingsController < ApplicationController
-    def index
-        @greetings = Greeting.all
-        render json: @greetings
-    end
+  def index
+    @greetings = Greeting.all
+    render json: @greetings
+  end
 
-    def create
-        @greetings = Greeting.new(greeting_params)
-        @greetings.save
-        render json: @grettings, status: :created
-    end
-    def show
-        render json: @greetings
-    end
+  def create
+    @greetings = Greeting.new(greeting_params)
+    @greetings.save
+    render json: @grettings, status: :created
+  end
+
+  def show
+    render json: @greetings
+  end
 end
